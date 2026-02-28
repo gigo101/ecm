4e0ad7da599bebecc89301cd35cac8901c7db252
Ian jay Padios <ianjaypadios@Ian-Jays-MacBook-Air.local>
2026-02-28 18:32:20 +0800

    Push updates

--- src/api.js
+++ src/api.js
@@ -25,8 +25,12 @@
 
 import axios from "axios";
 
+// const api = axios.create({
+//   baseURL: "http://127.0.0.1:8000",
+// });
+
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 // Auto-attach JWT
